a205231cabcd15d8c6e80bfdd65784941fe1f1f6
malinjr07 <malinjr07@gmail.com>
2023-01-24 15:33:59 +0600

    data in json format done

--- src/components/FileUploader.tsx
+++ src/components/FileUploader.tsx
@@ -1,14 +1,14 @@
 import Image from 'next/image';
-import React, { FC, useRef, useState } from 'react';
+import React, { FC, useRef } from 'react';
 
 type uploaderProps = {
   label?: string;
   identifier?: string;
   maxFileSizeInBytes?: number;
   updateFilesCb: (e: any[]) => void;
-  //   Type for all other dynamic props
   [x: string]: any;
   multiples?: boolean;
+  stateData: any[];
 };
 const KILO_BYTES_PER_BYTE = 1000;
 const convertBytesToKB = (bytes: number) =>
@@ -20,11 +20,9 @@ const FileUploader: FC<uploaderProps> = ({
   maxFileSizeInBytes = 50000000,
   updateFilesCb,
   multiples = true,
+  stateData,
   ...otherProps
 }) => {
-  // State to manage uploading files
-  const [files, setFiles] = useState<any>({});
-
   //   Ref To Handle additional click event to upload files
   const fileInputField = useRef<HTMLInputElement>(null);
 
@@ -32,54 +30,14 @@ const FileUploader: FC<uploaderProps> = ({
     fileInputField.current?.click();
   };
 
-  /**
-   * addNewFiles Function
-   * Take FileList as Arguments (we get FileList from File Input Field)
-   * Check if the file size meets  our condition first
-   * If it matches and the multiples props is false, then it'll return the first file of the array
-   * if the multiples value is true, then we will make an object, make a key value pair of File Name (Key) & file (value)
-   */
-
-  const addNewFiles = (newFiles: any) => {
-    let tempObj: any = {};
-    for (let file of newFiles) {
-      if (file.size <= maxFileSizeInBytes) {
-        if (!multiples) {
-          return { file };
-        }
-        tempObj[file.name] = file;
-      }
-    }
-    return tempObj;
-  };
-
-  /**
-   *
-   * @param nestedObj files
-   * @returns Array of the files
-   */
-  const convertNestedObjectToArray = (nestedObj: any) =>
-    Object.keys(nestedObj).map((key) => nestedObj[key]);
-
-  /**
-   *
-   * @param files Files
-   * make an array of files and send the array to the updating Callback Function
-   */
-  const callUpdateFilesCb = (files: any) => {
-    const filesAsArray = convertNestedObjectToArray(files);
-    updateFilesCb(filesAsArray);
-  };
-
-  const removeFile = (fileName: any) => {
-    delete files[fileName];
-    setFiles({ ...files });
-    callUpdateFilesCb({ ...files });
+  const removeFile = (fileObj: any) => {
+    const fileIndex = stateData.filter((e: any) => e.name !== fileObj.name);
+    updateFilesCb(fileIndex);
   };
 
   return (
     <div className='w-full flex flex-col justify-start items-start gap-4  '>
-      {Object.keys(files).length <= 0 ? (
+      {stateData.length <= 0 ? (
         <>
           {' '}
           <label htmlFor={identifier} className='capitalize pb-3 font-medium '>
@@ -108,15 +66,10 @@ const FileUploader: FC<uploaderProps> = ({
               {...otherProps}
               multiple={multiples}
               onChange={(e) => {
-                // Destruct the files object from e.target and rename it to newFiles
                 // If the Length of the newFiles (which is a fileList array) 0, then the boolean value will be false
-                const { files: newFiles } = e.target;
-                if (newFiles?.length) {
-                  let updatedFiles = addNewFiles(newFiles);
-
-                  // Update the State
-                  setFiles({ ...files, ...updatedFiles });
-                  callUpdateFilesCb(updatedFiles);
+                if (e.target.files?.length) {
+                  let updatedFiles = Object.values(e.target.files);
+                  updateFilesCb([...stateData, ...updatedFiles]);
                 }
               }}
             />
@@ -126,48 +79,38 @@ const FileUploader: FC<uploaderProps> = ({
         <div className=' w-full gap-5 flex flex-col justify-center items-center '>
           <h5 className='text-2xl text-rose-900 '>Selected Images</h5>
           <div className='w-full flex flex-wrap flex-row gap-9 '>
-            {Object.keys(files).map((fileName, index) => {
-              let file = files[fileName];
-              //   This variable will convert the image name into image URL
-              const imgUrl = URL.createObjectURL(file);
+            {stateData.map((files, index) => {
+              //   This variable will convert the image object into image URL
+              const imgUrl = URL.createObjectURL(files);
 
               /* Check If the File type is image or not. For image, the file type goes as "image/jpeg" || "image/png" || "image/webp" ; etc.
                */
-              let isImageFile = file.type.split('/')[0] === 'image';
+              let isImageFile = files.type.split('/')[0] === 'image';
               return (
                 <section key={index}>
-                  <div>
-                    {isImageFile && (
-                      //   <img
-                      //     src={URL.createObjectURL(file)}
-                      //     alt={`file preview ${index}`}
-                      //   />
-
-                      <div className='w-24 h-16 relative'>
-                        <Image
-                          src={imgUrl}
-                          alt='image'
-                          fill
-                          className='object-contain'
-                        />
-                        <button
-                          type='button'
-                          onClick={() => removeFile(fileName)}
-                          className='px-2 py-1 text-white absolute top-0 right-0 rounded-full bg-gray-600'
-                        >
-                          x
-                        </button>
-                      </div>
-                    )}
-                    <div
-                    // isImageFile={isImageFile}
-                    >
-                      <span>{file.name}</span>
-                      <aside>
-                        <span>{convertBytesToKB(file.size)} kb</span>
-                        <i className='fas fa-trash-alt' />
-                      </aside>
+                  {isImageFile && (
+                    <div className='w-24 h-16 relative'>
+                      <Image
+                        src={imgUrl}
+                        alt='image'
+                        fill
+                        className='object-contain'
+                      />
+                      <button
+                        type='button'
+                        onClick={() => removeFile(files)}
+                        className='px-2 py-1 text-white absolute top-0 right-0 rounded-full bg-gray-600'
+                      >
+                        x
+                      </button>
                     </div>
+                  )}
+                  <div>
+                    <span>{files.name}</span>
+                    <aside>
+                      <span>{convertBytesToKB(files.size)} kb</span>
+                      <i className='fas fa-trash-alt' />
+                    </aside>
                   </div>
                 </section>
               );
@@ -194,15 +137,10 @@ const FileUploader: FC<uploaderProps> = ({
                 {...otherProps}
                 multiple={multiples}
                 onChange={(e) => {
-                  // Destruct the files object from e.target and rename it to newFiles
                   // If the Length of the newFiles (which is a fileList array) 0, then the boolean value will be false
-                  const { files: newFiles } = e.target;
-                  if (newFiles?.length) {
-                    let updatedFiles = addNewFiles(newFiles);
-
-                    // Update the State
-                    setFiles({ ...files, ...updatedFiles });
-                    callUpdateFilesCb(updatedFiles);
+                  if (e.target.files?.length) {
+                    let updatedFiles = Object.values(e.target.files);
+                    updateFilesCb([...stateData, ...updatedFiles]);
                   }
                 }}
               />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,11 @@ const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   const [images, setImages] = useState<any[]>([]);
+  console.log('🚀 ~ file: index.tsx:9 ~ Home ~ images', images);
   return (
     <section className='w-screen h-screen flex justify-center items-center '>
       <form className='px-6 py-4 rounded-2xl border ' method='post'>
-        <FileUploader updateFilesCb={() => {}} />
+        <FileUploader updateFilesCb={(e) => setImages(e)} stateData={images} />
       </form>
     </section>
   );
