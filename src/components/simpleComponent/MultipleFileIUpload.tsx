ffeb24fd7728d3006074e4de6aee6c3e4de6bc34
malinjr07 <malinjr07@gmail.com>
2023-07-26 21:49:05 +0600

    project modified

---
+++ src/components/simpleComponent/MultipleFileIUpload.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+
+const MultipleFileUpload: FC = () => {
+  return <div>MultipleFileupload</div>;
+};
+
+export default MultipleFileUpload;
+
---
+++ src/components/simpleComponent/SingleFileUploader.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+const SingleFileUploader: FC = () => {
+  return <div>SingleFileUploader</div>;
+};
+
+export default SingleFileUploader;
